2b632fa82ac610e42a482397ab7893ffb5a4ee82
andras-zold <andras.zold@superwert.com>
2024-02-27 19:31:52 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [basket, setBasket] = useState({})
+
+  const addProduct = async () => {
+    await axios.post(
+      `https://abch.mithrandir.hu/hu/default/jsonapi/basket?related=product`,
+      {
+        data: {
+          attributes: {
+            'product.id': '25',
+          },
+        },
+      },
+      // {
+      //   withCredentials: true,
+      // },
+    )
+  }
+
+  useEffect(() => {
+    const fetchBasket = async () => {
+      const data = await axios.get(
+        `https://abch.mithrandir.hu/hu/default/jsonapi/basket`,
+        {
+          //withCredentials: true, // Include this if cookies are expected to be sent with the request
+        },
+      )
+      setBasket(data)
+    }
+    fetchBasket()
+  }, [])
+
+  if (!basket) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      <button onClick={() => addProduct()}>order product:25</button>
+      <pre>{JSON.stringify(basket, null, 2)}</pre>
+    </>
+  )
+}
+
+export default App
